f0eb29d66d076b294c3ea4d233129325f6795d1f
Michal Zlatkovský <michal@zlatkovsky.cz>
2025-06-05 12:45:40 +0200

    vite fix

--- kalkulacka-osvc/vite.config.ts
+++ kalkulacka-osvc/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dane-kalkulacky/",
+  base: "/osvc-kalkulacka/dist/",
   plugins: [react()],
   resolve: {
     alias: {
